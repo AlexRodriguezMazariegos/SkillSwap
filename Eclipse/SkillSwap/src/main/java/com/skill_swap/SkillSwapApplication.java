e31360fc7c774f94615cd5b459d9a1b159dc7a8f
WenbinLinEviden <Wenbin.lin.external@eviden.com>
2024-05-06 13:47:09 +0200

    Creacion del proyecto Spring y Angular

---
+++ Eclipse/SkillSwap/src/main/java/com/skill_swap/SkillSwapApplication.java
@@ -0,0 +1,13 @@
+package com.skill_swap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSwapApplication.class, args);
+	}
+
+}
---
+++ Eclipse/SkillSwap/src/test/java/com/skill_swap/SkillSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill_swap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
