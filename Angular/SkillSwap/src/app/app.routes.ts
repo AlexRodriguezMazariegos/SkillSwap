fefe4e7840ae87dc8554e5c240572fc1edfb75cd
Jorge Burgos <jorge.burgos.external@eviden.com>
2024-05-09 11:11:30 +0200

    Añadidos estilos  globales y carpeta assets

--- Angular/SkillSwap/src/app/app.routes.ts
+++ Angular/SkillSwap/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component:AppComponent
+    },
+    {
+        path: 'login', component:LoginComponent
+    },
+    {
+        path: 'home', component:HomeComponent
+    },
+];
