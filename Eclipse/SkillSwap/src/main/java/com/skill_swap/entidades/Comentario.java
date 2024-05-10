a75fa36560aeda8819a4071642bcc0a100f48f30
AdrianHZExternal <167760091+AdrianHZExternal@users.noreply.github.com>
2024-05-10 11:50:58 +0200

    entidades
    esta vez espero que bien

---
+++ Eclipse/SkillSwap/src/main/java/com/skill_swap/entidades/Comentario.java
@@ -0,0 +1,86 @@
+package atos.eviden.entidades;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Comentario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id_comentario;
+	
+	@ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario id_usuario;
+	
+	@ManyToOne
+    @JoinColumn(name = "id_publicacion")
+    private Publicacion id_publicacion;
+
+	
+	private Date fecha;
+	
+	private String texto;
+
+	public Comentario(Long id_comentario, Usuario id_usuario, Publicacion id_publicacion, Date fecha, String texto) {
+		super();
+		this.id_comentario = id_comentario;
+		this.id_usuario = id_usuario;
+		this.id_publicacion = id_publicacion;
+		this.fecha = fecha;
+		this.texto = texto;
+	}
+	
+	public Comentario() {
+		super();
+		
+	}
+
+	public Long getId_comentario() {
+		return id_comentario;
+	}
+
+	public void setId_comentario(Long id_comentario) {
+		this.id_comentario = id_comentario;
+	}
+
+	public Usuario getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(Usuario id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public Publicacion getId_publicacion() {
+		return id_publicacion;
+	}
+
+	public void setId_publicacion(Publicacion id_publicacion) {
+		this.id_publicacion = id_publicacion;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
+}
