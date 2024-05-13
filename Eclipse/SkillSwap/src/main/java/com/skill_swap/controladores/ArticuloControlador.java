5a28e13fb71f74b62d28bbf13b88d33418b23716
AndresBG95 <becker.salinas.external@eviden.com>
2024-05-13 11:23:09 +0200

    cambios
    imports ArticuloRepositorio, ArticuloServicio y ArticuloControlador

--- Eclipse/SkillSwap/src/main/java/com/skill_swap/controladores/ArticuloControlador.java
+++ Eclipse/SkillSwap/src/main/java/com/skill_swap/controladores/ArticuloControlador.java
@@ -2,8 +2,17 @@ package com.skill_swap.controladores;
 
 import com.skill_swap.entidades.Articulo;
 import com.skill_swap.servicios.ArticuloServicio;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
