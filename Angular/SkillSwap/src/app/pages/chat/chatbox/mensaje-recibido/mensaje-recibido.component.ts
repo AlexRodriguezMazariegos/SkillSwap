2e618d102c17f7291a2be4bcd7eec5079559623d
JuanRamosQuintana1 <juan.ramos.external@eviden.com>
2024-05-13 11:34:28 +0200

    x
    Mensaje recibido componente

---
+++ Angular/SkillSwap/src/app/pages/chat/chatbox/mensaje-recibido/mensaje-recibido.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mensaje-recibido',
+  standalone: true,
+  imports: [],
+  templateUrl: './mensaje-recibido.component.html',
+  styleUrl: './mensaje-recibido.component.css'
+})
+export class MensajeRecibidoComponent {
+
+}
