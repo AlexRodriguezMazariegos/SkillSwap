3139bae57ddb14912c64eef27028a9c3b12e8cc2
JuanRamosQuintana1 <juan.ramos.external@eviden.com>
2024-05-14 16:11:33 +0200

    GetUsuarioById

--- Angular/SkillSwap/src/app/pages/profile/user-info/user-info.component.ts
+++ Angular/SkillSwap/src/app/pages/profile/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './user-info.component.css'
 })
 export class UserInfoComponent {
+
+  @Input() miUsuario: any;
 
 }
