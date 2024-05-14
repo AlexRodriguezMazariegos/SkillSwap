2ca3da7e430fed987097ec0f50fceff368b09788
JuanRamosQuintana1 <juan.ramos.external@eviden.com>
2024-05-14 12:35:59 +0200

    Servicio usuarios

--- Angular/SkillSwap/src/app/pages/home/home.component.ts
+++ Angular/SkillSwap/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
 import { TarjetaUsuarioComponent } from "./tarjeta-usuario/tarjeta-usuario.component";
+import { usuario } from '../../model/usuario';
+import { UsuarioService } from '../../services/usuario.service';
 @Component({
     selector: 'app-home',
     standalone: true,
@@ -10,6 +12,15 @@ import { TarjetaUsuarioComponent } from "./tarjeta-usuario/tarjeta-usuario.compo
     styleUrl: './home.component.css',
     imports: [RouterModule, NavbarComponent, SidebarComponent, TarjetaUsuarioComponent]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    public misUsuarios:usuario[] = [];
+    constructor (private usuarioService:UsuarioService){}
+    
+    ngOnInit(): void {
+        this.usuarioService.getusuarios().subscribe((data:usuario[]) =>{
+            this.misUsuarios = data;
+            console.log(this.misUsuarios)
+        });
+    }
 
 }
