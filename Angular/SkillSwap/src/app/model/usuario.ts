fb8c6b189d50f567398a5b9d21f4d7963aee3db2
MiriamRamirez <167768641+MiriamRamirezSanchez@users.noreply.github.com>
2024-05-14 13:29:42 +0200

    Modelos del front completados

--- Angular/SkillSwap/src/app/model/usuario.ts
+++ Angular/SkillSwap/src/app/model/usuario.ts
@@ -1,12 +1,12 @@
-import { skill } from "./skill";
+import { skill } from './skill';
 
-export interface usuario{
-    id_usuario:number,
-    nombre:string,
-    apellido:string,
-    email:string,
-    contrasena:string,
-    urlGitHub:string,
-    puestoEmpresa:string,
-    skills:Array<skill>
+export interface usuario {
+  id_usuario: number;
+  nombre: string;
+  apellido: string;
+  email: string;
+  contrasena: string;
+  urlGitHub: string;
+  puestoEmpresa: string;
+  skills: Array<skill>;
 }
