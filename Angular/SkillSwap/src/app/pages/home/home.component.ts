67623bb4a692f99c74c4117fcadff6102d745624
JorgeBurgosB <jorge.burgos.external@eviden.com>
2024-05-16 11:11:10 +0200

    Articulos añadidos

--- Angular/SkillSwap/src/app/pages/home/home.component.ts
+++ Angular/SkillSwap/src/app/pages/home/home.component.ts
@@ -5,21 +5,29 @@ import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
 import { TarjetaUsuarioComponent } from "./tarjeta-usuario/tarjeta-usuario.component";
 import { usuario } from '../../model/usuario';
 import { UsuarioService } from '../../services/usuario.service';
+import { ArticuloService } from '../../services/articulo.service';
+import { ArticuloComponent } from "./articulo/articulo.component";
+import { articulo } from '../../model/articulo';
+import { log } from 'console';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [RouterModule, NavbarComponent, SidebarComponent, TarjetaUsuarioComponent]
+    imports: [RouterModule, NavbarComponent, SidebarComponent, TarjetaUsuarioComponent, ArticuloComponent]
 })
 export class HomeComponent implements OnInit {
     public misUsuarios:usuario[] = [];
-    constructor (private usuarioService:UsuarioService){}
+    public articulos:articulo[] = [];
+    constructor (private usuarioService:UsuarioService, private articuloService:ArticuloService){}
     
     ngOnInit(): void {
         this.usuarioService.getusuarios().subscribe((data:usuario[]) =>{
             this.misUsuarios = data;
-            console.log(this.misUsuarios)
+        });
+        this.articuloService.getArticulos().subscribe((data:articulo[])=> {
+            this.articulos = data;
+            console.log(this.articulos);
         });
     }
 
