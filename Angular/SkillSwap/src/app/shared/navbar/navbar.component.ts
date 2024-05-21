87d818f262eaa9431efd86f3ae91bc99b7208013
Alex <alejandro.rodriguezm.external@eviden.com>
2024-05-21 12:25:29 +0200

    boton profile navarbar

--- Angular/SkillSwap/src/app/shared/navbar/navbar.component.ts
+++ Angular/SkillSwap/src/app/shared/navbar/navbar.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit{
-  
-  usuario = localStorage.getItem('usuario')
-  nombreUsuario = ''
-  imagenUsuario = ''
-  
+export class NavbarComponent implements OnInit {
+  usuario = localStorage.getItem('usuario');
+  nombreUsuario = '';
+  imagenUsuario = '';
+
+  constructor(public dialog: MatDialog, private router: Router) {}
+
   ngOnInit(): void {
-  
     if (this.usuario) {
       const currentUser = JSON.parse(this.usuario);
-      this.nombreUsuario = currentUser.nombre
-      this.imagenUsuario = currentUser.fotoDePerfil
+      this.nombreUsuario = currentUser.nombre;
+      this.imagenUsuario = currentUser.fotoDePerfil;
       console.log();
-      
-    } 
-}
+    }
+  }
+  abrirProfile() {
+    if (this.usuario) {
+      const currentUser = JSON.parse(this.usuario);
+      console.log(currentUser.id);
+      this.router.navigate([`/profile/${currentUser.id}`]);
+    } else {
+      this.router.navigate([``]);
+    }
+  }
 }
