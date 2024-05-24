6a98101a67557296264b03e34b749b2c361835ed
JorgeBurgosB <jorge.burgos.external@eviden.com>
2024-05-24 12:47:50 +0200

    Boton de seguir y siguiendo funcionales

--- Angular/SkillSwap/src/app/services/seguimiento/seguimiento.service.ts
+++ Angular/SkillSwap/src/app/services/seguimiento/seguimiento.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { seguimiento } from '../../model/seguimiento';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,23 @@ export class SeguimientoService {
   baseUrl = 'http://localhost:8080/api/v1/seguimiento';
 
   getSeguidos(id: number):Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/seguidos/${id}`)
+    return this.http.get<any>(`${this.baseUrl}/seguidores/${id}`)
   }
 
   getSeguidores(id: number):Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/seguidores/${id}`)
+    return this.http.get<any>(`${this.baseUrl}/seguidos/${id}`)
   }
+
+  postSeguimiento(seguimiento: seguimiento):Observable<any> {
+    return this.http.post<any>(this.baseUrl, seguimiento)
+  }
+
+  getSeguimiento(idSeguidor:number, idSeguido:number):Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${idSeguidor}/${idSeguido}`)
+  }
+
+  deleteSeguimiento(idSeguidor:number, idSeguido:number):Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/${idSeguidor}/${idSeguido}`)
+  }
+
 }
