ec3f5e03f7c8a6f1c8c74536277f92698f8619e9
AndresBG95 <becker.salinas.external@eviden.com>
2024-05-28 08:57:58 +0200

    chat
    chat

--- Eclipse/SkillSwap/src/main/java/com/skill_swap/repositorios/UsuarioRepositorio.java
+++ Eclipse/SkillSwap/src/main/java/com/skill_swap/repositorios/UsuarioRepositorio.java
@@ -14,5 +14,8 @@ public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
 	
 	@Query("Select u from Usuario u where u.email = ?1 and u.contrasena =?2")
 	Usuario login(String email, String contrasena);
+
+	@Query("Select u from Usuario u where u.nombre = ?1")
+	Usuario findByNombre(String nombre);
 	
 }
