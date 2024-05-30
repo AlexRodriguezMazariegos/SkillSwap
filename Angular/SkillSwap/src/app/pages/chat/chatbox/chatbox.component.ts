aaf10ba17bb6b1894233681d6836d47e327b8103
AndresBG95 <becker.salinas.external@eviden.com>
2024-05-30 11:46:54 +0200

    cambios chat front

--- Angular/SkillSwap/src/app/pages/chat/chatbox/chatbox.component.ts
+++ Angular/SkillSwap/src/app/pages/chat/chatbox/chatbox.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatMessage } from '../../../model/chat-mensaje';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ChatService } from '../../../services/chat/chat.service';
+
+
+
+
+
+
 
 @Component({
     selector: 'app-chatbox',
     standalone: true,
     templateUrl: './chatbox.component.html',
     styleUrl: './chatbox.component.css',
-    imports: []
+    imports: [FormsModule],
+    
 })
-export class ChatboxComponent {
+export class ChatboxComponent implements OnInit {
 
-}
+    messageInput: string = '';
+    userId: string = '';
+    messageList: any[] = [];
+   
+    // route: any;
+    // chatService: any;
+  
+    constructor(private chatService: ChatService, private route: ActivatedRoute) { }
+  
+    ngOnInit(): void {
+      this.userId = this.route.snapshot.params["userId"];
+      this.chatService.joinRoom("ABC");
+      console.log('*** UserId: ' + this.userId);
+      this.listenerMessage();
+    }
+  
+    sendMessage() {
+      console.log('**send UserId: ' + this.userId);
+      const chatMessage = {
+        message: this.messageInput,
+        user: this.userId
+      } as ChatMessage
+      this.chatService.sendMessage("ABC", chatMessage);
+      this.messageInput = '';
+    }
+  
+    listenerMessage() {
+      this.chatService.getMessageSubject().subscribe((messages: any) => {
+        this.messageList = messages.map((item: any) => ({
+          ...item,
+          message_side: item.user === this.userId ? 'sender' : 'receiver'
+        }))
+      });
+    }
+  }
+  
--- Angular/SkillSwap/src/app/services/chat/chat.service.ts
+++ Angular/SkillSwap/src/app/services/chat/chat.service.ts
@@ -10,41 +10,41 @@ import { ChatMessage } from '../../model/chat-mensaje';
 })
 export class ChatService {
 
-  private stompClient: any
-  private messageSubject: BehaviorSubject<ChatMessage[]>=new BehaviorSubject<ChatMessage[]>([]);
+  private stompClient: any;
+  private messageSubject: BehaviorSubject<ChatMessage[]> = new BehaviorSubject<ChatMessage[]>([]);
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
     this.iniConnectionSocket();
   }
 
-  iniConnectionSocket(){
-    const url='//localhost:8080/chat-socket';
-    const socket= new SockJS(url);
-    this.stompClient=Stomp.over(socket)
+  iniConnectionSocket() {
+    const url = '//localhost:8080/api/v1/chat/chat-socket';
+    const socket = new SockJS(url);
+    this.stompClient = Stomp.over(socket);
   }
 
-  joinRoom(roomId:string){
-    this.stompClient.connect({},()=>{
-      this.stompClient.subscribe(`/topic/${roomId}`, (messages:any)=>{
-        const messageContent =JSON.parse(messages.body);
-        const currentMessage = this.messageSubject.getValue();
-        currentMessage.push(messageContent);
-        this.messageSubject.next(currentMessage);
-      })
+  joinRoom(roomId: string) {
+    this.stompClient.connect({}, () => {
+      this.stompClient.subscribe(`/topic/${roomId}`, (message: any) => {
+        const messageContent = JSON.parse(message.body);
+        const currentMessages = this.messageSubject.getValue();
+        currentMessages.push(messageContent);
+        this.messageSubject.next(currentMessages);
+      });
       this.loadMessagesFromDatabase(roomId);
-    })
+    });
   }
 
-  senMessage(roomId: string, chatMessage: ChatMessage){
-    this.stompClient.send(`/app/chat/${roomId}`,{},JSON.stringify(chatMessage))
-  } 
+  sendMessage(roomId: string, chatMessage: ChatMessage) {
+    this.stompClient.send(`/app/chat/${roomId}`, {}, JSON.stringify(chatMessage));
+  }
 
-  getMessageSubject(){
+  getMessageSubject() {
     return this.messageSubject.asObservable();
   }
 
   loadMessagesFromDatabase(roomId: string) {
-    this.http.get<ChatMessage[]>(`http://localhost:8080/chat/history/${roomId}`).subscribe(messages => {
+    this.http.get<ChatMessage[]>(`http://localhost:8080/api/v1/chat/history/${roomId}`).subscribe(messages => {
       this.messageSubject.next(messages);
     });
   }
