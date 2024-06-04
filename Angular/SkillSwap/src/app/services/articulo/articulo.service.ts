d08af94315f82f0f21e0c6998c83ec4af20d5153
Alex <alejandro.rodriguezm.external@eviden.com>
2024-06-04 10:09:46 +0200

    repaso de codigo

--- Angular/SkillSwap/src/app/services/articulo/articulo.service.ts
+++ Angular/SkillSwap/src/app/services/articulo/articulo.service.ts
@@ -12,48 +12,57 @@ export class ArticuloService {
   private headers!: HttpHeaders;
   baseUrl = 'http://localhost:8080/api/v1/articulo';
 
-  constructor(private http: HttpClient, private authservice: AuthService) { 
-    
+  constructor(private http: HttpClient, private authservice: AuthService) {
     this.headers = this.addAuthorizationHeader();
-  
-}
-
-private addAuthorizationHeader(): HttpHeaders {
-  let token = this.authservice.token;
-  if (token != null) {
-    return this.httpHeaders.append('Authorization', 'Bearer ' + token);
   }
-  return this.httpHeaders;
-}
+
+  private addAuthorizationHeader(): HttpHeaders {
+    let token = this.authservice.token;
+    if (token != null) {
+      return this.httpHeaders.append('Authorization', 'Bearer ' + token);
+    }
+    return this.httpHeaders;
+  }
   getArticulos(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/activos`,{ headers: this.headers});
+    return this.http.get<any>(`${this.baseUrl}/activos`, {
+      headers: this.headers,
+    });
   }
 
   getArticuloCualquieraById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/${id}`,{ headers: this.headers });
+    return this.http.get<any>(`${this.baseUrl}/${id}`, {
+      headers: this.headers,
+    });
   }
 
   getArticuloById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/activos/${id}`,{ headers: this.headers });
+    return this.http.get<any>(`${this.baseUrl}/activos/${id}`, {
+      headers: this.headers,
+    });
   }
 
   getArticuloByUserId(id: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/usuario/${id}`,{ headers: this.headers });
+    return this.http.get<any>(`${this.baseUrl}/usuario/${id}`, {
+      headers: this.headers,
+    });
   }
 
-  postArticulo(newArticulo: articulo):Observable<any>{
-    return this.http.post(this.baseUrl, newArticulo,{ headers: this.headers })
+  postArticulo(newArticulo: articulo): Observable<any> {
+    return this.http.post(this.baseUrl, newArticulo, { headers: this.headers });
   }
 
   activarDesactivar(id: number): Observable<any> {
-    return this.http.patch(`${this.baseUrl}/${id}`,{ headers: this.headers });
+    return this.http.patch(`${this.baseUrl}/${id}`, { headers: this.headers });
   }
 
   deleteArticulo(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/${id}`,{ headers: this.headers })
+    return this.http.delete<any>(`${this.baseUrl}/${id}`, {
+      headers: this.headers,
+    });
   }
-    updateArticulo(id: number, articulo: articulo): Observable<articulo> {
-    return this.http.put<articulo>(`${this.baseUrl}/${id}`, articulo,{ headers: this.headers } );
+  updateArticulo(id: number, articulo: articulo): Observable<articulo> {
+    return this.http.put<articulo>(`${this.baseUrl}/${id}`, articulo, {
+      headers: this.headers,
+    });
   }
-
 }
--- Angular/SkillSwap/src/app/services/chat/chat.service.ts
+++ Angular/SkillSwap/src/app/services/chat/chat.service.ts
@@ -6,46 +6,53 @@ import { HttpClient } from '@angular/common/http';
 import { ChatMessage } from '../../model/chat-mensaje';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
+  private stompClient: any;
+  private messageSubject: BehaviorSubject<ChatMessage[]> = new BehaviorSubject<
+    ChatMessage[]
+  >([]);
 
-  private stompClient: any
-  private messageSubject: BehaviorSubject<ChatMessage[]>=new BehaviorSubject<ChatMessage[]>([]);
-
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
     this.iniConnectionSocket();
   }
 
-  iniConnectionSocket(){
-    const url='//localhost:8080/chat-socket';
-    const socket= new SockJS(url);
-    this.stompClient=Stomp.over(socket)
+  iniConnectionSocket() {
+    const url = '//localhost:8080/chat-socket';
+    const socket = new SockJS(url);
+    this.stompClient = Stomp.over(socket);
   }
 
-  joinRoom(roomId:string){
-    this.stompClient.connect({},()=>{
-      this.stompClient.subscribe(`/topic/${roomId}`, (messages:any)=>{
-        const messageContent =JSON.parse(messages.body);
+  joinRoom(roomId: string) {
+    this.stompClient.connect({}, () => {
+      this.stompClient.subscribe(`/topic/${roomId}`, (messages: any) => {
+        const messageContent = JSON.parse(messages.body);
         const currentMessage = this.messageSubject.getValue();
         currentMessage.push(messageContent);
         this.messageSubject.next(currentMessage);
-      })
+      });
       this.loadMessagesFromDatabase(roomId);
-    })
+    });
   }
 
-  senMessage(roomId: string, chatMessage: ChatMessage){
-    this.stompClient.send(`/app/chat/${roomId}`,{},JSON.stringify(chatMessage))
-  } 
+  senMessage(roomId: string, chatMessage: ChatMessage) {
+    this.stompClient.send(
+      `/app/chat/${roomId}`,
+      {},
+      JSON.stringify(chatMessage)
+    );
+  }
 
-  getMessageSubject(){
+  getMessageSubject() {
     return this.messageSubject.asObservable();
   }
 
   loadMessagesFromDatabase(roomId: string) {
-    this.http.get<ChatMessage[]>(`http://localhost:8080/chat/history/${roomId}`).subscribe(messages => {
-      this.messageSubject.next(messages);
-    });
+    this.http
+      .get<ChatMessage[]>(`http://localhost:8080/chat/history/${roomId}`)
+      .subscribe((messages) => {
+        this.messageSubject.next(messages);
+      });
   }
 }
